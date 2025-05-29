5c16079e683998bd8e0994dcca072fe15d9f04d6
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-29 15:50:22 -0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
 
 export default function App() {
   
   return (
-    <main>
-      <p>Game goes here</p>
-    </main>
+      <main>   
+        <Header />
+        <section className='section-status'>
+          <p>Game goes here</p>
+        </section>
+      </main>
   )
 }
