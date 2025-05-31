593724a2a9329639bda70209b9fa905043c3ff9d
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-30 18:13:40 -0700

    Added language chips

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import { languagesData } from './languages';
 
 export default function App() {
   
+const languagesElement = languagesData.map(language => (
+  <span
+    key={language.name}
+    style={{
+      backgroundColor: language.backgroundColor,
+      color: language.color
+    }}
+    className='span-languages'
+  >
+    {language.name}
+  </span>
+))
+
   return (
       <main>   
         <Header />
         <section className='section-game-status'>
           <h2>You win!</h2>
           <p>Well done! 🎉</p>
+        </section>
+        <section className='section-language-container'>
+            {languagesElement}
         </section>
       </main>
   )
