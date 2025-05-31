a4df4dea7fa072fdbe9141e515aee73c5d601b8d
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-05-31 14:06:42 -0700

    Add word display

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import Header from './components/Header';
 import { languagesData } from './languages';
 
 export default function App() {
-  
-const languagesElement = languagesData.map(language => (
-  <span
-    key={language.name}
-    style={{
-      backgroundColor: language.backgroundColor,
-      color: language.color
-    }}
-    className='span-languages'
-  >
-    {language.name}
-  </span>
-))
+  const [currentWord, setCurrentWord] = useState('react') //initialize state
+ 
+  const languagesElement = languagesData.map(language => (
+    <span
+      key={language.name}
+      style={{
+        backgroundColor: language.backgroundColor,
+        color: language.color
+      }}
+      className='span-languages'
+    >
+      {language.name}
+    </span>
+  ))
+
+  //turn the current word into an array, and map over the letters, set the index as the key prop
+  const letterElements = [...currentWord].map((letter, index) => (
+    <span key={index} className='span-current-word'>
+      {letter}
+    </span>
+  ))
 
   return (
       <main>   
@@ -27,6 +35,9 @@ const languagesElement = languagesData.map(language => (
         </section>
         <section className='section-language-container'>
             {languagesElement}
+        </section>
+        <section className='section-word-display'>
+            {letterElements}
         </section>
       </main>
   )
